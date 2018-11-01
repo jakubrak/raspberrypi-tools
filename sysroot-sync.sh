320ddbaae93ff1fee9c543121dd6e060fe010ec4
Jakub Rak <rakjak2@gmail.com>
2018-11-01 15:56:57 +0100

    Initial commit

---
+++ sysroot-sync.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# check if number of mandatory args is correct
+if [ "$#" -ne 1 ]; then
+    echo Usage: `basename "$0"` SYSROOT_DIR
+    exit 1
+fi
+
+SYSROOT_DIR=$1
+
+# check if directory exists
+if [ ! -d "${SYSROOT_DIR}" ]; then
+    echo ${SYSROOT_DIR} does not exist!
+    exit 1
+fi
+
+echo ${SYSROOT_DIR}
+
+rsync -avz pi@raspberrypi.local:/lib ${SYSROOT_DIR}
+rsync -avz pi@raspberrypi.local:/usr/include ${SYSROOT_DIR}/usr
+rsync -avz pi@raspberrypi.local:/usr/lib ${SYSROOT_DIR}/usr
+rsync -avz pi@raspberrypi.local:/opt/vc ${SYSROOT_DIR}/opt
